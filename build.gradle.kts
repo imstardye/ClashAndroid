31f7243ac7f0243cf39f2fa20936a00b6cc2aeda
GitHub Action <action@github.com>
2025-10-22 03:51:22 +0000

    Bump version to 1.2.7 (102007)

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "1.2.5"
-            versionCode = 102005
+            versionName = "1.2.7"
+            versionCode = 102007
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
